c0f88409e29183f254fc4fbfe3b3197d837c7efe
goaud <goaud@users.noreply.github.com>
2026-04-21 22:58:25 +0200

    style: the lines of code are rearranged

--- src/BinaryToInteger.java
+++ src/BinaryToInteger.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 
 class binaryToInteger {
     public static void main(String[] args) {
-
         int binNum;
         int rest;
         int ans = 0;
         int hoch = 1;
-
         Scanner a = new Scanner(System.in);
         System.out.print("Enter a binary number: ");
-
         binNum = a.nextInt();
-
         while (binNum != 0) {
             rest = binNum % 2;
             binNum = binNum / 10;
--- src/IntegerToBinary.java
+++ src/IntegerToBinary.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 
 class integerToBinary {
     public static void main(String[] args) {
-
         int dezNum;
         int rest;
         int ans = 0;
         int hoch = 1;
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a decimal number: ");
-
         dezNum = scanner.nextInt();  // 5 / 2 / 1
-
         while (dezNum > 0) {
             rest = dezNum % 2; // 1 / 0 / 1
             dezNum = dezNum / 2; // 2 / 1 / 0
