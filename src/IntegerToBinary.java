efac49431c0541ee070363e4ad53b69c618da07d
Viktor Halupka <bigdatalag@gmail.com>
2026-04-28 08:56:46 +0200

    style: more descriptive variable names

--- src/IntegerToBinary.java
+++ src/IntegerToBinary.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-class integerToBinary {
+class IntegerToBinary {
     public static void main(String[] args) {
-        int dezNum;
-        int rest;
-        int ans = 0;
+        int dezimalNum;
+        int restwert;
+        int antwort = 0;
         int hoch = 1;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a decimal number: ");
-        dezNum = scanner.nextInt();  // 5 / 2 / 1
-        while (dezNum > 0) {
-            rest = dezNum % 2; // 1 / 0 / 1
-            dezNum = dezNum / 2; // 2 / 1 / 0
-            ans += hoch * rest; // 1 / 0 / 100
+        dezimalNum = scanner.nextInt();  // 5 / 2 / 1
+        while (dezimalNum > 0) {
+            restwert = dezimalNum % 2; // 1 / 0 / 1
+            dezimalNum = dezimalNum / 2; // 2 / 1 / 0
+            antwort += hoch * restwert; // 1 / 0 / 100
             hoch *= 10; // 10 / 100 / 1000
         }
-        System.out.println(ans); // 1 + 0 + 100 = 101
+        System.out.println(antwort); // 1 + 0 + 100 = 101
         scanner.close();
     }
 }
