8957dedf02934cf6353f0a364404f127c4dc6271
Viktor Halupka <bigdatalag@gmail.com>
2026-04-28 09:09:12 +0200

    style: rename binary input variable

--- src/BinaryToInteger.java
+++ src/BinaryToInteger.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 class BinaryToInteger {
     public static void main(String[] args) {
-        int binarNum;
+        int binareNum;
         int restwert;
         int antwort = 0;
         int hoch = 1;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a binary number: ");
-        binarNum = scanner.nextInt();
-        while (binarNum != 0) {
-            restwert = binarNum % 2;
-            binarNum = binarNum / 10;
+        binareNum = scanner.nextInt();
+        while (binareNum != 0) {
+            restwert = binareNum % 2;
+            binareNum = binareNum / 10;
             antwort += hoch * restwert;
             hoch *= 2;
         }
